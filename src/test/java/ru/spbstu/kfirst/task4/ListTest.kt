12e207bfaf48e95829ad052b1ec20c40040b820b
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-06-01 19:45:22 +0300

    Task 4 (lists): convert to numeral system

--- src/test/java/ru/spbstu/kfirst/task4/ListTest.kt
+++ src/test/java/ru/spbstu/kfirst/task4/ListTest.kt
@@ -18,4 +18,21 @@ class ListTest {
         assertEquals("3*5*5", factorizeToString(75))
         assertEquals("2*3*3*19", factorizeToString(342))
     }
+
+    @Test
+    fun convert() {
+        assertEquals(listOf(1), convert(1, 2))
+        assertEquals(listOf(1, 2, 1, 0), convert(100, 4))
+        assertEquals(listOf(1, 3, 12), convert(250, 14))
+        assertEquals(listOf(2, 14, 12), convert(1000, 19))
+    }
+
+    @Test
+    fun convertToString() {
+        assertEquals("1", convertToString(1, 2))
+        assertEquals("1210", convertToString(100, 4))
+        assertEquals("13c", convertToString(250, 14))
+        assertEquals("2ec", convertToString(1000, 19))
+        assertEquals("z", convertToString(35, 36))
+    }
 }
