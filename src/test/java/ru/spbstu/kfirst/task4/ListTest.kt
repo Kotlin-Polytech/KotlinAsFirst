4ea1bcfd6353f7c69c4949db2642ea2ff7ec4b72
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2016-06-02 17:45:52 +0300

    Minor refactoring (task 4)

--- src/test/java/ru/spbstu/kfirst/task4/ListTest.kt
+++ src/test/java/ru/spbstu/kfirst/task4/ListTest.kt
@@ -6,6 +6,53 @@ import org.junit.Assert.*
 
 class ListTest {
     @Test
+    fun mean() {
+        assertEquals(0.0, mean(listOf()), 1e-5)
+        assertEquals(1.0, mean(listOf(1.0)), 1e-5)
+        assertEquals(2.0, mean(listOf(3.0, 1.0, 2.0)), 1e-5)
+        assertEquals(3.0, mean(listOf(0.0, 2.0, 7.0, 8.0, -2.0)), 1e-5)
+    }
+
+    @Test
+    fun center() {
+        assertEquals(listOf<Double>(), center(arrayListOf()))
+        assertEquals(listOf(0.0), center(arrayListOf(3.14)))
+        assertEquals(listOf(1.0, -1.0, 0.0), center(arrayListOf(3.0, 1.0, 2.0)))
+        assertEquals(listOf(-3.0, -1.0, 4.0, 5.0, -5.0), center(arrayListOf(0.0, 2.0, 7.0, 8.0, -2.0)))
+    }
+
+    @Test
+    fun times() {
+        assertEquals(0.0, times(listOf(), listOf()), 1e-5)
+        assertEquals(-5.0, times(listOf(1.0, -4.0), listOf(3.0, 2.0)), 1e-5)
+        assertEquals(-19.0, times(listOf(-1.0, 2.0, -3.0), listOf(3.0, -2.0, 4.0)), 1e-5)
+    }
+
+    @Test
+    fun abs() {
+        assertEquals(0.0, abs(listOf()), 1e-5)
+        assertEquals(3.0, abs(listOf(3.0)), 1e-5)
+        assertEquals(5.0, abs(listOf(3.0, -4.0)), 1e-5)
+        assertEquals(8.774964, abs(listOf(4.0, -5.0, 6.0)), 1e-5)
+    }
+
+    @Test
+    fun polynom() {
+        assertEquals(0.0, polynom(listOf(), 1000.0), 1e-5)
+        assertEquals(42.0, polynom(listOf(42.0), -1000.0), 1e-5)
+        assertEquals(13.0, polynom(listOf(3.0, 2.0), 5.0), 1e-5)
+        assertEquals(0.0, polynom(listOf(2.0, -3.0, 1.0), 1.0), 1e-5)
+        assertEquals(45.0, polynom(listOf(-7.0, 6.0, 4.0, -4.0, 1.0), -2.0), 1e-5)
+    }
+
+    @Test
+    fun accumulate() {
+        assertEquals(listOf<Double>(), accumulate(arrayListOf()))
+        assertEquals(listOf(3.14), accumulate(arrayListOf(3.14)))
+        assertEquals(listOf(1.0, 3.0, 6.0, 10.0), accumulate(arrayListOf(1.0, 2.0, 3.0, 4.0)))
+    }
+
+    @Test
     fun factorize() {
         assertEquals(listOf(2), factorize(2))
         assertEquals(listOf(3, 5, 5), factorize(75))
@@ -51,52 +98,5 @@ class ListTest {
         assertEquals(250, decimalFromString("13c", 14))
         assertEquals(1000, decimalFromString("2ec", 19))
         assertEquals(35, decimalFromString("z", 36))
-    }
-
-    @Test
-    fun mean() {
-        assertEquals(0.0, mean(listOf()), 1e-5)
-        assertEquals(1.0, mean(listOf(1.0)), 1e-5)
-        assertEquals(2.0, mean(listOf(3.0, 1.0, 2.0)), 1e-5)
-        assertEquals(3.0, mean(listOf(0.0, 2.0, 7.0, 8.0, -2.0)), 1e-5)
-    }
-
-    @Test
-    fun center() {
-        assertEquals(listOf<Double>(), center(arrayListOf()))
-        assertEquals(listOf(0.0), center(arrayListOf(3.14)))
-        assertEquals(listOf(1.0, -1.0, 0.0), center(arrayListOf(3.0, 1.0, 2.0)))
-        assertEquals(listOf(-3.0, -1.0, 4.0, 5.0, -5.0), center(arrayListOf(0.0, 2.0, 7.0, 8.0, -2.0)))
-    }
-
-    @Test
-    fun times() {
-        assertEquals(0.0, times(listOf(), listOf()), 1e-5)
-        assertEquals(-5.0, times(listOf(1.0, -4.0), listOf(3.0, 2.0)), 1e-5)
-        assertEquals(-19.0, times(listOf(-1.0, 2.0, -3.0), listOf(3.0, -2.0, 4.0)), 1e-5)
-    }
-
-    @Test
-    fun abs() {
-        assertEquals(0.0, abs(listOf()), 1e-5)
-        assertEquals(3.0, abs(listOf(3.0)), 1e-5)
-        assertEquals(5.0, abs(listOf(3.0, -4.0)), 1e-5)
-        assertEquals(8.774964, abs(listOf(4.0, -5.0, 6.0)), 1e-5)
-    }
-
-    @Test
-    fun polynom() {
-        assertEquals(0.0, polynom(listOf(), 1000.0), 1e-5)
-        assertEquals(42.0, polynom(listOf(42.0), -1000.0), 1e-5)
-        assertEquals(13.0, polynom(listOf(3.0, 2.0), 5.0), 1e-5)
-        assertEquals(0.0, polynom(listOf(2.0, -3.0, 1.0), 1.0), 1e-5)
-        assertEquals(45.0, polynom(listOf(-7.0, 6.0, 4.0, -4.0, 1.0), -2.0), 1e-5)
-    }
-
-    @Test
-    fun accumulate() {
-        assertEquals(listOf<Double>(), accumulate(arrayListOf()))
-        assertEquals(listOf(3.14), accumulate(arrayListOf(3.14)))
-        assertEquals(listOf(1.0, 3.0, 6.0, 10.0), accumulate(arrayListOf(1.0, 2.0, 3.0, 4.0)))
     }
 }
