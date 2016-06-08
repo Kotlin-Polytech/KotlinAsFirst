9ce367fb473b82c065a3df106ac6d6441253c273
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2016-06-08 10:40:18 +0300

    Task 5 (parsing): dates

---
+++ src/test/java/ru/spbstu/kfirst/task5/ParseTest.kt
@@ -0,0 +1,25 @@
+package ru.spbstu.kfirst.task5
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ParseTest {
+    @Test
+    fun dateStrToDigit() {
+        assertEquals("15.07.2016", dateStrToDigit("15 июля 2016"))
+        assertEquals("", dateStrToDigit("3 мартобря 1918"))
+        assertEquals("18.11.2018", dateStrToDigit("18 ноября 2018"))
+        assertEquals("", dateStrToDigit("23"))
+        assertEquals("03.04.2011", dateStrToDigit("3 апреля 2011"))
+    }
+
+    @Test
+    fun dateDigitToStr() {
+        assertEquals("15 июля 2016", dateDigitToStr("15.07.2016"))
+        assertEquals("", dateDigitToStr("01.02.20.19"))
+        assertEquals("", dateDigitToStr("28.00.2000"))
+        assertEquals("3 апреля 2011", dateDigitToStr("03.04.2011"))
+        assertEquals("", dateDigitToStr("ab.cd.ef"))
+    }
+}
