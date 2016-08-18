36b082c7015e75ee236ac5d88a3799ac234d82b9
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-08-18 10:51:29 +0300

    Chapter 3: break / continue / some details

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -23,10 +23,14 @@ fun isPrime(n: Int): Boolean {
     return true
 }
 
+/*
+ * Пример: проверка на совершенность
+ */
 fun isPerfect(n: Int): Boolean {
     var sum = 1
     for (m in 2..n/2) {
-        if (n % m == 0) sum += m
+        if (n % m > 0) continue
+        sum += m
         if (sum > n) break
     }
     return sum == n
