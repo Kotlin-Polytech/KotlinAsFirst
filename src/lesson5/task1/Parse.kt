2e8b3bb6980daf732d65c97c9453e640997c36d6
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2016-08-29 22:55:55 +0300

    Chapter 5: string formatting

--- src/lesson5/task1/Parse.kt
+++ src/lesson5/task1/Parse.kt
@@ -17,6 +17,21 @@ fun timeStrToSeconds(str: String): Int {
     return result
 }
 
+fun twoDigitStr(n: Int) = if (n in 0..9) "0$n" else "$n"
+
+/**
+ * Пример
+ *
+ * Дано seconds -- время в секундах, прошедшее с начала дня.
+ * Вернуть текущее время в виде строки в формате "ЧЧ:ММ:СС".
+ */
+fun timeSecondsToStr(seconds: Int): String {
+    val hour = seconds / 3600
+    val minute = (seconds % 3600) / 60
+    val second = seconds % 60
+    return String.format("%02d:%02d:%02d", hour, minute, second)
+}
+
 /**
  * Средняя
  *
