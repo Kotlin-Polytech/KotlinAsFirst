1e44e493e0b32c2471200cedbb4028acfe8e48d6
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2016-08-30 19:03:00 +0300

    m

--- test/lesson8/task1/Tests.kt
+++ test/lesson8/task1/Tests.kt
@@ -38,21 +38,21 @@ class Tests {
     fun markdownToHtmlSimple() {
         markdownToHtmlSimple("input/markdown_simple.md", "temp.html")
 
-        val result = File("temp.html").readText().replace("\\s+", " ").replace("[\\t\\n]", " ").trim()
+        val result = File("temp.html").readText().replace(Regex("[\\s\\n\\t]"), "")
         val expected =
-            """
-            <html>
-                <body>
-                    <p>
-                        Lorem ipsum <i>dolor sit amet</i>, consectetur <b>adipiscing</b> elit.
-                        Vestibulum lobortis. <s>Est vehicula rutrum <i>suscipit</i></s>, ipsum <s>lib</s>ero <i>placerat <b>tortor</b></i>.
-                    </p>
-                    <p>
-                        Suspendisse <s>et elit in enim tempus iaculis</s>.
-                    </p>
-                </body>
-            </html>
-            """.trimIndent().replace("\\s+", " ").replace("[\\t\\n]", " ").trim()
+                """
+                <html>
+                    <body>
+                        <p>
+                            Lorem ipsum <i>dolor sit amet</i>, consectetur <b>adipiscing</b> elit.
+                            Vestibulum lobortis. <s>Est vehicula rutrum <i>suscipit</i></s>, ipsum <s>lib</s>ero <i>placerat <b>tortor</b></i>.
+                        </p>
+                        <p>
+                            Suspendisse <s>et elit in enim tempus iaculis</s>.
+                        </p>
+                    </body>
+                </html>
+                """.trimIndent().replace(Regex("[\\s\\n\\t]"), "")
         assertEquals(expected, result)
 
         File("temp.html").delete()
@@ -61,110 +61,56 @@ class Tests {
     @Test
     @Tag("Hard")
     fun markdownToHtmlLists() {
-        markdownToHtmlSimple("input/markdown_lists.md", "temp.html")
+        markdownToHtmlLists("input/markdown_lists.md", "temp.html")
 
-        val result = File("temp.html").readText().replace("\\s+", " ").replace("[\\t\\n]", " ").trim()
+        val result = File("temp.html").readText().replace(Regex("[\\s\\n\\t]"), "")
         val expected =
-            """
-            <html>
-              <body>
-                <ul>
-                  <li>
-                    Утка по-пекински
+                """
+                <html>
+                  <body>
                     <ul>
-                      <li>Утка</li>
-                      <li>Соус</li>
-                    </ul>
-                  </li>
-                  <li>
-                    Салат Оливье
-                    <ol>
-                      <li>Мясо
+                      <li>
+                        Утка по-пекински
                         <ul>
-                          <li>
-                              Или колбаса
-                          </li>
+                          <li>Утка</li>
+                          <li>Соус</li>
                         </ul>
                       </li>
-                      <li>Майонез</li>
-                      <li>Картофель</li>
-                      <li>Что-то там ещё</li>
-                    </ol>
-                  </li>
-                  <li>Помидоры</li>
-                  <li>
-                    Яблоки
-                    <ol>
-                      <li>Красные</li>
-                      <li>Зелёные</li>
-                    </ol>
-                  </li>
-                </ul>
-              </body>
-            </html>
-            """.trimIndent().replace("\\s+", " ").replace("[\\t\\n]", " ").trim()
+                      <li>
+                        Салат Оливье
+                        <ol>
+                          <li>Мясо
+                            <ul>
+                              <li>
+                                  Или колбаса
+                              </li>
+                            </ul>
+                          </li>
+                          <li>Майонез</li>
+                          <li>Картофель</li>
+                          <li>Что-то там ещё</li>
+                        </ol>
+                      </li>
+                      <li>Помидоры</li>
+                      <li>
+                        Фрукты
+                        <ol>
+                          <li>Бананы</li>
+                          <li>
+                            Яблоки
+                            <ol>
+                              <li>Красные</li>
+                              <li>Зелёные</li>
+                            </ol>
+                          </li>
+                        </ol>
+                      </li>
+                    </ul>
+                  </body>
+                </html>
+                """.trimIndent().replace(Regex("[\\s\\n\\t]"), "")
         assertEquals(expected, result)
 
-        File("temp.html").delete()
-    }
-
-    @Test
-    @Tag("Impossible")
-    fun markdownToHtml() {
-        markdownToHtml("input/markdown.md", "temp.html")
-
-        val result = File("temp.html").readText().replace("\\s+", " ").replace("[\\t\\n]", " ").trim()
-        val expected =
-            """
-            <html>
-                <body>
-                    <p>
-                        <ul>
-                            <li>Lorem <b>ipsum</b> dolor sit amet, consectetur adipiscing elit.</li>
-                            <li>Nullam<i>ac</i><b>lectus</b> nec lectus congue sollicitudin vel vel eros.</li>
-                        </ul>
-                    </p>
-                    <p>
-                        <s>
-                            Phasellus accumsan enim eget tellus gravida, quis lacinia urna facilisis.
-                            Donec tristique risus sed nisl congue pulvinar.
-                            Ut vel odio convallis, pulvinar arcu semper, interdum lectus.
-                        <s>
-                    </p>
-                    <p>
-                        Mauris dictum nisl vitae odio sagittis, id aliquam nisl posuere.
-                        Duis vel sapien dignissim, fermentum ante at, porttitor turpis.
-                    </p>
-                    <p>
-                        <ol>
-                            <li>Duis vestibulum libero nec suscipit lobortis.</li>
-                            <li><i>Ut</i> sollicitudin lectus ac dapibus semper.
-                                <ul>
-                                    <li>Lists</li>
-                                    <li>Irony</li>
-                                    <li>Markdown</li>
-                                    <li>HTML</li>
-                                </ul>
-                            </li>
-                            <li>Vivamus aliquam enim quis lectus dapibus pellentesque vel nec nulla.</li>
-                        </ol>
-                    </p>
-                    <p>
-                        <ol>
-                            <li>Proin convallis arcu ac ante placerat, ac lobortis tellus eleifend.</li>
-                            <li>Nunc sit amet nulla facilisis orci congue vestibulum.</li>
-                            <li>Aliquam suscipit velit dictum cursus accumsan.</li>
-                        </ol>
-                    </p>
-                    <p>
-                        Fusce lacinia orci vel massa porta, at egestas lorem tempor.
-                        Fusce blandit sem sed urna facilisis efficitur.
-                        Nulla pulvinar lacus eget nunc aliquam, vitae hendrerit tortor condimentum.
-                    </p>
-                </body>
-            </html>
-            """.trimIndent().replace("\\s+", " ").replace("[\\t\\n]", " ").trim()
-        assertEquals(expected, result)
         File("temp.html").delete()
     }
 
