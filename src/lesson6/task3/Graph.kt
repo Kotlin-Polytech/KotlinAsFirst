33e1a873e0578c8012b10fa34b0120fbfefa9848
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2016-09-05 09:54:54 +0300

    Lesson 6: graph examples refactoring

--- src/lesson6/task3/Graph.kt
+++ src/lesson6/task3/Graph.kt
@@ -2,67 +2,69 @@ package lesson6.task3
 
 import java.util.*
 
-data class Vertex(val name: String) {
-    val neighbors = mutableSetOf<Vertex>()
-}
-
 class Graph {
+    private data class Vertex(val name: String) {
+        val neighbors = mutableSetOf<Vertex>()
+    }
+
     private val vertices = mutableMapOf<String, Vertex>()
 
-    operator fun get(name: String) = vertices[name] ?: throw IllegalArgumentException()
+    private operator fun get(name: String) = vertices[name] ?: throw IllegalArgumentException()
 
     fun addVertex(name: String) {
         vertices[name] = Vertex(name)
     }
 
-    fun connect(first: Vertex, second: Vertex) {
+    private fun connect(first: Vertex, second: Vertex) {
         first.neighbors.add(second)
         second.neighbors.add(first)
     }
 
     fun connect(first: String, second: String) = connect(this[first], this[second])
-}
 
-/**
- * Пример
- *
- * По двум вершинам рассчитать расстояние между ними = число дуг на самом коротком пути между ними.
- * Вернуть -1, если пути между вершинами не существует.
- *
- * Используется поиск в ширину
- */
-fun bfs(start: Vertex, finish: Vertex): Int {
-    val queue = ArrayDeque<Vertex>()
-    queue.add(start)
-    val visited = mutableMapOf(start to 0)
-    while (queue.isNotEmpty()) {
-        val next = queue.poll()
-        val distance = visited[next]!!
-        if (next == finish) return distance
-        for (neighbor in next.neighbors) {
-            if (neighbor in visited) continue
-            visited.put(neighbor, distance + 1)
-            queue.add(neighbor)
+    /**
+     * Пример
+     *
+     * По двум вершинам рассчитать расстояние между ними = число дуг на самом коротком пути между ними.
+     * Вернуть -1, если пути между вершинами не существует.
+     *
+     * Используется поиск в ширину
+     */
+    fun bfs(start: String, finish: String) = bfs(this[start], this[finish])
+
+    private fun bfs(start: Vertex, finish: Vertex): Int {
+        val queue = ArrayDeque<Vertex>()
+        queue.add(start)
+        val visited = mutableMapOf(start to 0)
+        while (queue.isNotEmpty()) {
+            val next = queue.poll()
+            val distance = visited[next]!!
+            if (next == finish) return distance
+            for (neighbor in next.neighbors) {
+                if (neighbor in visited) continue
+                visited.put(neighbor, distance + 1)
+                queue.add(neighbor)
+            }
         }
+        return -1
     }
-    return -1
+
+    /**
+     * Пример
+     *
+     * По двум вершинам рассчитать расстояние между ними = число дуг на самом коротком пути между ними.
+     * Вернуть -1, если пути между вершинами не существует.
+     *
+     * Используется поиск в глубину
+     */
+    fun dfs(start: String, finish: String): Int = dfs(this[start], this[finish], setOf()) ?: -1
+
+    private fun dfs(start: Vertex, finish: Vertex, visited: Set<Vertex>): Int? =
+            if (start == finish) 0
+            else {
+                val min = start.neighbors.filter { it !in visited }
+                        .map { dfs(it, finish, visited + start) }
+                        .filterNotNull().min()
+                if (min == null) null else min + 1
+            }
 }
-
-/**
- * Пример
- *
- * По двум вершинам рассчитать расстояние между ними = число дуг на самом коротком пути между ними.
- * Вернуть -1, если пути между вершинами не существует.
- *
- * Используется поиск в глубину
- */
-fun dfs(start: Vertex, finish: Vertex): Int = dfs(start, finish, setOf()) ?: -1
-
-fun dfs(start: Vertex, finish: Vertex, visited: Set<Vertex>): Int? =
-        if (start == finish) 0
-        else {
-            val min = start.neighbors.filter { it !in visited }
-                    .map { dfs(it, finish, visited + start) }
-                    .filterNotNull().min()
-            if (min == null) null else min + 1
-        }
