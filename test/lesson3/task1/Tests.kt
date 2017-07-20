40fa56d5dd29366041805c1bbf1ea32d19465248
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-07-20 18:50:44 +0300

    Lesson 3: better tests for sin() and cos()

--- test/lesson3/task1/Tests.kt
+++ test/lesson3/task1/Tests.kt
@@ -149,6 +149,7 @@ class Tests {
         assertEquals(1.0, sin(Math.PI / 2.0, 1e-5), 1e-5)
         assertEquals(0.0, sin(Math.PI, 1e-5), 1e-5)
         assertEquals(-1.0, sin(3.0 * Math.PI / 2.0, 1e-5), 1e-5)
+        assertEquals(0.0, sin(100 * Math.PI, 1e-5), 1e-5)
     }
 
     @Test
@@ -158,6 +159,7 @@ class Tests {
         assertEquals(0.0, cos(Math.PI / 2.0, 1e-5), 1e-5)
         assertEquals(-1.0, cos(Math.PI, 1e-5), 1e-5)
         assertEquals(0.0, cos(3.0 * Math.PI / 2.0, 1e-5), 1e-5)
+        assertEquals(1.0, cos(100 * Math.PI, 1e-5), 1e-5)
     }
 
     @Test
