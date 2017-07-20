cd628bb2f1f28845142228c31433ceea6efaa791
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-07-20 18:48:37 +0300

    Lesson 3: better tests for maxDivisor()

--- test/lesson3/task1/Tests.kt
+++ test/lesson3/task1/Tests.kt
@@ -118,6 +118,10 @@ class Tests {
         assertEquals(59, maxDivisor(177))
         assertEquals(17, maxDivisor(34))
         assertEquals(7, maxDivisor(49))
+        assertEquals(509, maxDivisor(8653))
+        assertEquals(1 , maxDivisor(2124679 ))
+        assertEquals(1, maxDivisor(1073676287))
+        assertEquals(1, maxDivisor(Int.MAX_VALUE))
     }
 
     @Test
