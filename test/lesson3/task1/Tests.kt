9a469baf14559c7de72a7cf955eb1897146145b6
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-07-20 17:18:53 +0300

    Lesson 3: better tests for lcm()

--- test/lesson3/task1/Tests.kt
+++ test/lesson3/task1/Tests.kt
@@ -89,6 +89,11 @@ class Tests {
         assertEquals(8, lcm(2, 8))
         assertEquals(24, lcm(6, 8))
         assertEquals(975, lcm(39, 75))
+        assertEquals(13384091, lcm(1357, 9863))
+        assertEquals(1339310349, lcm(13579, 98631))
+        assertEquals(2089830349, lcm(23579, 88631))
+        assertEquals(2022222222, lcm(2, 1011111111))
+        assertEquals(2022222222, lcm(1011111111, 2))
     }
 
     @Test
