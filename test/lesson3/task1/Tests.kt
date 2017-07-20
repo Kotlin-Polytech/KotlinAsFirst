4153323069c889f6c8986a73b21380c9967043c9
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-07-20 18:58:38 +0300

    Lesson 3: better tests for isCoPrime()

--- test/lesson3/task1/Tests.kt
+++ test/lesson3/task1/Tests.kt
@@ -131,6 +131,8 @@ class Tests {
         assertFalse(isCoPrime(6, 8))
         assertTrue(isCoPrime(17, 97))
         assertFalse(isCoPrime(37, 111))
+        assertTrue(isCoPrime(1234567890, 908765431))
+        assertTrue(isCoPrime(2109876543, 1234567891))
     }
 
     @Test
