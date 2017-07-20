e1f9907f85b6a00273e8a9868ea44c8f6b508a07
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-07-20 16:31:32 +0300

    Enhance tests for lesson 2

--- test/lesson2/task1/Tests.kt
+++ test/lesson2/task1/Tests.kt
@@ -19,9 +19,11 @@ class Tests {
     @Test
     @Tag("Easy")
     fun ageDescription() {
+        assertEquals("1 год", ageDescription(1))
         assertEquals("21 год", ageDescription(21))
         assertEquals("132 года", ageDescription(132))
         assertEquals("12 лет", ageDescription(12))
+        assertEquals("111 лет", ageDescription(111))
         assertEquals("199 лет", ageDescription(199))
     }
 
