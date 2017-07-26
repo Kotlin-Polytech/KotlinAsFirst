a3c277ce616200cf0b6b466a97d14f04a86f92c2
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-07-26 20:54:22 +0300

    Lesson 4: more tests for factorizeToString

--- test/lesson4/task1/Tests.kt
+++ test/lesson4/task1/Tests.kt
@@ -160,6 +160,9 @@ class Tests {
         assertEquals("2", factorizeToString(2))
         assertEquals("3*5*5", factorizeToString(75))
         assertEquals("2*3*3*19", factorizeToString(342))
+        assertEquals("7*7*31*31*151*151", factorizeToString(1073676289))
+        assertEquals("1073676287", factorizeToString(1073676287))
+        assertEquals(Int.MAX_VALUE.toString(), factorizeToString(Int.MAX_VALUE))
     }
 
     @Test
