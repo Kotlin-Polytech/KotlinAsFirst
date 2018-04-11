5997a9dbd210d155d57b3f1c9a8ce3e7a3c6d595
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2018-04-11 20:03:05 +0300

    dateStrToDigit/dateDigitToStr + new daysInMonth task

--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -31,6 +31,23 @@ class Tests {
     }
 
     @Test
+    @Tag("Easy")
+    fun daysInMonth() {
+        assertEquals(31, daysInMonth(1, 1990))
+        assertEquals(28, daysInMonth(2, 1990))
+        assertEquals(31, daysInMonth(3, 1990))
+        assertEquals(30, daysInMonth(4, 1990))
+        assertEquals(31, daysInMonth(5, 1990))
+        assertEquals(30, daysInMonth(6, 1990))
+        assertEquals(31, daysInMonth(7, 1990))
+        assertEquals(31, daysInMonth(8, 1990))
+        assertEquals(29, daysInMonth(2, 1992))
+        assertEquals(29, daysInMonth(2, 1996))
+        assertEquals(28, daysInMonth(2, 1900))
+        assertEquals(29, daysInMonth(2, 2000))
+    }
+
+    @Test
     @Tag("Normal")
     fun dateStrToDigit() {
         assertEquals("15.07.2016", dateStrToDigit("15 июля 2016"))
