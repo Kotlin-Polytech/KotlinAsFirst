57aaddd106f1a5799d29b64b94877562aeb97b9a
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2018-04-11 20:06:13 +0300

    daysInMonth to lesson2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -28,6 +28,15 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  */
 fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
 
+
+/**
+ * Простая
+ *
+ * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
+ * Вернуть число дней в этом месяце этого года по григорианскому календарю.
+ */
+fun daysInMonth(month: Int, year: Int): Int = TODO()
+
 /**
  * Средняя
  *
--- test/lesson2/task2/Tests.kt
+++ test/lesson2/task2/Tests.kt
@@ -29,6 +29,23 @@ class Tests {
         assertTrue(queenThreatens(8, 1, 1, 8))
         assertFalse(queenThreatens(7, 6, 5, 7))
     }
+    
+    @Test
+    @Tag("Easy")
+    fun daysInMonth() {
+        assertEquals(31, daysInMonth(1, 1990))
+        assertEquals(28, daysInMonth(2, 1990))
+        assertEquals(31, daysInMonth(3, 1990))
+        assertEquals(30, daysInMonth(4, 1990))
+        assertEquals(31, daysInMonth(5, 1990))
+        assertEquals(30, daysInMonth(6, 1990))
+        assertEquals(31, daysInMonth(7, 1990))
+        assertEquals(31, daysInMonth(8, 1990))
+        assertEquals(29, daysInMonth(2, 1992))
+        assertEquals(29, daysInMonth(2, 1996))
+        assertEquals(28, daysInMonth(2, 1900))
+        assertEquals(29, daysInMonth(2, 2000))
+    }
 
     @Test
     @Tag("Normal")
