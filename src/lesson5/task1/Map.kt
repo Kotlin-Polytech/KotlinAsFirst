e1119fd657a66e75613156222d6ce09ffa87fe41
ice-phoenix <Marat.Akhin@gmail.com>
2018-06-07 20:46:05 +0300

    Start work on chapter 05

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -1,3 +1,18 @@
 package lesson5.task1
 
+fun shoppingListCost(
+        shoppingList: List<String>,
+        costs: Map<String, Double>): Double {
+    var totalCost = 0.0
+
+    for (item in shoppingList) {
+        val itemCost = costs[item]
+        if (itemCost != null) {
+            totalCost += itemCost
+        }
+    }
+
+    return totalCost
+}
+
 // TODO: map tasks
--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -1,5 +1,37 @@
 package lesson5.task1
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
 class Tests {
+    @Test
+    fun shoppingListCostTest() {
+        val itemCosts = mapOf(
+                "Хлеб" to 50.0,
+                "Молоко" to 100.0
+        )
+        assertEquals(
+                150.0,
+                shoppingListCost(
+                        listOf("Хлеб", "Молоко"),
+                        itemCosts
+                )
+        )
+        assertEquals(
+                150.0,
+                shoppingListCost(
+                        listOf("Хлеб", "Молоко", "Кефир"),
+                        itemCosts
+                )
+        )
+        assertEquals(
+                0.0,
+                shoppingListCost(
+                        listOf("Хлеб", "Молоко", "Кефир"),
+                        mapOf()
+                )
+        )
+    }
+
     // TODO: map task tests
 }
