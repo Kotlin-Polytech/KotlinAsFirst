4746525d2867d6af95736e4b7717e4061f19d071
ice-phoenix <Marat.Akhin@gmail.com>
2018-06-09 16:06:12 +0300

    Continue work on chapter 05

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -1,5 +1,12 @@
 package lesson5.task1
 
+/**
+ * Пример
+ *
+ * Для заданного списка покупок `shoppingList` посчитать его общую стоимость
+ * на основе цен из `costs`. В случае неизвестной цены считать, что товар
+ * игнорируется.
+ */
 fun shoppingListCost(
         shoppingList: List<String>,
         costs: Map<String, Double>): Double {
@@ -13,6 +20,27 @@ fun shoppingListCost(
     }
 
     return totalCost
+}
+
+/**
+ * Пример
+ *
+ *
+ */
+fun filterByCountryCode(
+        phoneBook: MutableMap<String, String>,
+        countryCode: String) {
+    val namesToRemove = mutableListOf<String>()
+
+    for ((name, phone) in phoneBook) {
+        if (!phone.startsWith(countryCode)) {
+            namesToRemove.add(name)
+        }
+    }
+
+    for (name in namesToRemove) {
+        phoneBook.remove(name)
+    }
 }
 
 // TODO: map tasks
--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -1,10 +1,12 @@
 package lesson5.task1
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 
 class Tests {
     @Test
+    @Tag("Example")
     fun shoppingListCostTest() {
         val itemCosts = mapOf(
                 "Хлеб" to 50.0,
@@ -31,6 +33,24 @@ class Tests {
                         mapOf()
                 )
         )
+    }
+
+    @Test
+    fun filterByCountryCode() {
+        val phoneBook = mutableMapOf(
+                "Quagmire" to "+1-800-555-0143",
+                "Adam's Ribs" to "+82-000-555-2960",
+                "Pharmakon Industries" to "+1-800-555-6321"
+        )
+        
+        filterByCountryCode(phoneBook, "+1")
+        assertEquals(2, phoneBook.size)
+
+        filterByCountryCode(phoneBook, "+1")
+        assertEquals(2, phoneBook.size)
+
+        filterByCountryCode(phoneBook, "+999")
+        assertEquals(0, phoneBook.size)
     }
 
     // TODO: map task tests
