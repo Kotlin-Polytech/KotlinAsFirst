2c5dd39a92d76e3fb5e4708939123596bc56a772
Mikhail Glukhikh <glukhikh@mail.ru>
2018-08-22 10:12:04 +0300

    Lesson 1: add a test (Coursera sync)

--- test/lesson1/task1/Tests.kt
+++ test/lesson1/task1/Tests.kt
@@ -33,6 +33,7 @@ class Tests {
     @Test
     @Tag("Example")
     fun quadraticEquationRoot() {
+        assertEquals(2.0, quadraticEquationRoot(1.0, -3.0, 2.0), 1e-13)
         assertEquals(1.0, quadraticEquationRoot(1.0, -2.0, 1.0), 1e-13)
         assertEquals(-3.0, quadraticEquationRoot(1.0, 6.0, 9.0), 1e-13)
     }
