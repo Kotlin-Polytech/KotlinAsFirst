6712b6c575e94145397eafb3f52093c1571624ff
Mikhail Glukhikh <glukhikh@mail.ru>
2018-10-08 11:45:09 +0300

    Lesson 1 rename: grad -> deg

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -75,7 +75,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
 
 /**
  * Тривиальная
