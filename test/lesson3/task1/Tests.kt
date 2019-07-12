4500bda3927d41b5e07dc0d8b0b456c502e3cf47
Mikhail Glukhikh <glukhikh@mail.ru>
2019-07-12 10:28:31 +0300

    Forbid using standard implementations of sin/cos in lesson 3 fixing #66

--- test/lesson3/task1/Tests.kt
+++ test/lesson3/task1/Tests.kt
@@ -178,6 +178,8 @@ class Tests {
         assertEquals(0.0, sin(PI, 1e-5), 1e-5)
         assertEquals(-1.0, sin(3.0 * PI / 2.0, 1e-5), 1e-5)
         assertEquals(0.0, sin(100 * PI, 1e-5), 1e-5)
+        assertNotEquals(kotlin.math.sin(1.0), sin(1.0, 1.0))
+        assertNotEquals(kotlin.math.sin(-0.5), sin(-0.5, 1.0))
     }
 
     @Test
@@ -188,6 +190,8 @@ class Tests {
         assertEquals(-1.0, cos(PI, 1e-5), 1e-5)
         assertEquals(0.0, cos(3.0 * PI / 2.0, 1e-5), 1e-5)
         assertEquals(1.0, cos(100 * PI, 1e-5), 1e-5)
+        assertNotEquals(kotlin.math.cos(1.0), cos(1.0, 1.0))
+        assertNotEquals(kotlin.math.cos(0.5), cos(-0.5, 1.0))
     }
 
     @Test
