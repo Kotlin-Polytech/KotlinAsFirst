d5a607e3e6ad583ec871251b6c25552c7d1b2802
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-07-19 23:29:39 +0300

    Add stub for hexagonal variant of geometry lesson (#68)

---
+++ test/lesson8/task1/HexTests.kt
@@ -0,0 +1,62 @@
+package lesson8.task1
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+
+class HexTests {
+
+    @Test
+    @Tag("Normal")
+    fun hexPointDistance() {
+        assertEquals(5, HexPoint(6, 1).distance(HexPoint(1, 4)))
+    }
+
+    @Test
+    @Tag("Normal")
+    fun hexagonDistance() {
+        assertEquals(2, Hexagon(HexPoint(1, 3), 1).distance(Hexagon(HexPoint(6, 2), 2)))
+    }
+
+    @Test
+    @Tag("Trivial")
+    fun hexagonContains() {
+        assertTrue(Hexagon(HexPoint(3, 3), 1).contains(HexPoint(2, 3)))
+        assertFalse(Hexagon(HexPoint(3, 3), 1).contains(HexPoint(4, 4)))
+    }
+
+    @Test
+    @Tag("Easy")
+    fun hexSegmentValid() {
+        assertTrue(HexSegment(HexPoint(1, 3), HexPoint(5, 3)).isValid())
+        assertTrue(HexSegment(HexPoint(3, 1), HexPoint(3, 6)).isValid())
+        assertTrue(HexSegment(HexPoint(1, 5), HexPoint(4, 2)).isValid())
+        assertFalse(HexSegment(HexPoint(3, 1), HexPoint(6, 2)).isValid())
+    }
+
+    @Test
+    @Tag("Hard")
+    fun hexagonByThreePoints() {
+        assertEquals(
+            Hexagon(HexPoint(4, 2), 2),
+            hexagonByThreePoints(HexPoint(3, 1), HexPoint(2, 3), HexPoint(4, 4))
+        )
+        assertNull(
+            hexagonByThreePoints(HexPoint(3, 1), HexPoint(2, 3), HexPoint(5, 4))
+        )
+        assertEquals(
+            3,
+            hexagonByThreePoints(HexPoint(2, 3), HexPoint(3, 3), HexPoint(5, 3))?.radius
+        )
+    }
+
+    @Test
+    @Tag("Impossible")
+    fun minContainingHexagon() {
+        val points = arrayOf(HexPoint(3, 1), HexPoint(3, 2), HexPoint(5, 4), HexPoint(8, 1))
+        val result = minContainingHexagon(*points)
+        assertEquals(3, result.radius)
+        assertTrue(points.all { result.contains(it) })
+    }
+
+}
