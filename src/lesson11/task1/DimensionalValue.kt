54546ed88741798afb6967ff4d8adf2af7fa5055
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-07-21 21:01:03 +0300

    Lesson 11 (data classes): add task about dimensional value

---
+++ src/lesson11/task1/DimensionalValue.kt
@@ -0,0 +1,91 @@
+@file:Suppress("UNUSED_PARAMETER")
+
+package lesson11.task1
+
+/**
+ * Класс "Величина с размерностью".
+ *
+ * Предназначен для представления величин вроде "6 метров" или "3 килограмма"
+ * Общая сложность задания - средняя.
+ * Величины с размерностью можно складывать, вычитать, делить, менять им знак.
+ * Их также можно умножать и делить на число.
+ *
+ * В конструктор передаётся вещественное значение и строковая размерность.
+ * Строковая размерность может:
+ * - либо строго соответствовать одной из abbreviation класса Dimension (m, g)
+ * - либо соответствовать одной из приставок, к которой приписана сама размерность (Km, Kg, mm, mg)
+ * - во всех остальных случаях следует бросить IllegalArgumentException
+ */
+class DimensionalValue(value: Double, dimension: String) : Comparable<DimensionalValue> {
+    /**
+     * Величина с БАЗОВОЙ размерностью (например для 1.0Kg следует вернуть результат в граммах -- 1000.0)
+     */
+    val value: Double get() = TODO()
+
+    /**
+     * БАЗОВАЯ размерность (опять-таки для 1.0Kg следует вернуть GRAM)
+     */
+    val dimension: Dimension get() = TODO()
+
+    /**
+     * Конструктор из строки. Формат строки: значение пробел размерность (1 Kg, 3 mm, 100 g и так далее).
+     */
+    constructor(s: String) : this(TODO(), TODO())
+
+    /**
+     * Сложение с другой величиной. Если базовая размерность разная, бросить IllegalArgumentException
+     * (нельзя складывать метры и килограммы)
+     */
+    operator fun plus(other: DimensionalValue): DimensionalValue = TODO()
+
+    /**
+     * Смена знака величины
+     */
+    operator fun unaryMinus(): DimensionalValue = TODO()
+
+    /**
+     * Вычитание другой величины. Если базовая размерность разная, бросить IllegalArgumentException
+     */
+    operator fun minus(other: DimensionalValue): DimensionalValue = TODO()
+
+    /**
+     * Умножение на число
+     */
+    operator fun times(other: Double): DimensionalValue = TODO()
+
+    /**
+     * Деление на число
+     */
+    operator fun div(other: Double): DimensionalValue = TODO()
+
+    /**
+     * Деление на другую величину. Если базовая размерность разная, бросить IllegalArgumentException
+     */
+    operator fun div(other: DimensionalValue): Double = TODO()
+
+    /**
+     * Сравнение на равенство
+     */
+    override fun equals(other: Any?): Boolean = TODO()
+
+    /**
+     * Сравнение на больше/меньше. Если базовая размерность разная, бросить IllegalArgumentException
+     */
+    override fun compareTo(other: DimensionalValue): Int = TODO()
+}
+
+/**
+ * Размерность. В этот класс можно добавлять новые варианты (секунды, амперы, прочие), но нельзя убирать
+ */
+enum class Dimension(val abbreviation: String) {
+    METER("m"),
+    GRAM("g");
+}
+
+/**
+ * Приставка размерности. Опять-таки можно добавить новые варианты (деци-, санти-, мега-, ...), но нельзя убирать
+ */
+enum class DimensionPrefix(val abbreviation: String, val multiplier: Double) {
+    KILO("K", 1000.0),
+    MILLI("m", 0.001);
+}
---
+++ test/lesson11/task1/DimensionalValueTest.kt
@@ -0,0 +1,90 @@
+package lesson11.task1
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Tag
+
+internal class DimensionalValueTest {
+
+    private fun assertApproxEquals(expected: DimensionalValue, actual: DimensionalValue, eps: Double) {
+        assertEquals(expected.dimension, actual.dimension)
+        assertEquals(expected.value, actual.value, eps)
+    }
+
+    @Test
+    @Tag("Normal")
+    fun base() {
+        val first = DimensionalValue(1.0, "Kg")
+        assertEquals(1.0, first.value)
+        assertEquals(Dimension.GRAM, first.dimension)
+        val second = DimensionalValue("200 m")
+        assertEquals(200.0, second.value)
+        assertEquals(Dimension.METER, second.dimension)
+    }
+
+    @Test
+    @Tag("Easy")
+    fun plus() {
+        assertApproxEquals(DimensionalValue("2 Km"), DimensionalValue("1 Km") + DimensionalValue("1000 m"), 1e-8)
+        assertThrows(IllegalArgumentException::class.java) {
+            DimensionalValue("1 g") + DimensionalValue("1 m")
+        }
+    }
+
+    @Test
+    @Tag("Easy")
+    operator fun unaryMinus() {
+        assertApproxEquals(DimensionalValue("2 g"), DimensionalValue("-2 g"), 1e-12)
+    }
+
+    @Test
+    @Tag("Easy")
+    fun minus() {
+        assertApproxEquals(DimensionalValue("0 m"), DimensionalValue("1 Km") - DimensionalValue("1000 m"), 1e-10)
+        assertThrows(IllegalArgumentException::class.java) {
+            DimensionalValue("1 g") - DimensionalValue("1 m")
+        }
+    }
+
+    @Test
+    @Tag("Easy")
+    fun times() {
+        assertApproxEquals(DimensionalValue("2 Kg"), DimensionalValue("2 g") * 1000.0, 1e-8)
+    }
+
+    @Test
+    @Tag("Easy")
+    fun divValue() {
+        assertEquals(1.0, DimensionalValue("3 m") / DimensionalValue("3000 mm"), 1e-10)
+        assertThrows(IllegalArgumentException::class.java) {
+            DimensionalValue("1 g") / DimensionalValue("1 m")
+        }
+    }
+
+    @Test
+    @Tag("Easy")
+    fun divDouble() {
+        assertApproxEquals(DimensionalValue("42 mm"), DimensionalValue("42 m") / 1000.0, 1e-11)
+    }
+
+    @Test
+    @Tag("Easy")
+    fun equals() {
+        assertEquals(DimensionalValue("1 Kg"), DimensionalValue("1 Kg"))
+        assertEquals(DimensionalValue("3 mm"), DimensionalValue("3 mm"))
+    }
+
+    @Test
+    @Tag("Easy")
+    fun hashCodeTest() {
+        assertEquals(DimensionalValue("1 Kg").hashCode(), DimensionalValue("1 Kg").hashCode())
+    }
+
+    @Test
+    @Tag("Easy")
+    fun compareTo() {
+        assertTrue(DimensionalValue("1 Kg") < DimensionalValue("1500 g"))
+        assertTrue(DimensionalValue("1 m") > DimensionalValue("900 mm"))
+    }
+}
