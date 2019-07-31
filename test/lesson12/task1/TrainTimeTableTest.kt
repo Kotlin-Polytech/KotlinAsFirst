4a02685faaf20c05ab440bc9fe5ee22a7e155a38
Mikhail Glukhikh <glukhikh@mail.ru>
2019-07-31 11:19:26 +0300

    Lesson 12 (data containers): add train timetable task & stub for tests

---
+++ test/lesson12/task1/TrainTimeTableTest.kt
@@ -0,0 +1,32 @@
+package lesson12.task1
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class TrainTimeTableTest {
+
+    @Test
+    fun addTrain() {
+    }
+
+    @Test
+    fun removeTrain() {
+    }
+
+    @Test
+    fun addStop() {
+    }
+
+    @Test
+    fun removeStop() {
+    }
+
+    @Test
+    fun trains() {
+    }
+
+    @Test
+    fun testEquals() {
+    }
+}
