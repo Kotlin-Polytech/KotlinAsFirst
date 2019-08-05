deb13efd346e8eb7a10d7162e5d998b7da2d6207
ice-phoenix <Marat.Akhin@gmail.com>
2019-08-05 18:45:30 +0300

    Better examples for lesson6.flattenPhoneNumber

--- test/lesson6/task1/Tests.kt
+++ test/lesson6/task1/Tests.kt
@@ -60,8 +60,11 @@ class Tests {
     fun flattenPhoneNumber() {
         assertEquals("+79211234567", flattenPhoneNumber("+7 (921) 123-45-67"))
         assertEquals("123456798", flattenPhoneNumber("12 --  34- 5 -- 67 -98"))
-        assertEquals("", flattenPhoneNumber("ab-123"))
         assertEquals("+12345", flattenPhoneNumber("+12 (3) 4-5"))
+        assertEquals("", flattenPhoneNumber("+12 () 4-5"))
+        assertEquals("+425667", flattenPhoneNumber("+42 56 -- 67"))
+        assertEquals("+42566789", flattenPhoneNumber("+42(56 -- 67)89"))
+        assertEquals("", flattenPhoneNumber("ab-123"))
         assertEquals("", flattenPhoneNumber("134_+874"))
     }
 
