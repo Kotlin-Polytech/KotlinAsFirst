6a401a922f9cd8d3b597e9e5097b7375e536249c
Mikhail Glukhikh <glukhikh@mail.ru>
2020-02-12 17:04:41 +0300

    Lesson 11: fix DimensionalValue unaryMinus() test

--- test/lesson11/task1/DimensionalValueTest.kt
+++ test/lesson11/task1/DimensionalValueTest.kt
@@ -35,7 +35,7 @@ internal class DimensionalValueTest {
     @Test
     @Tag("Easy")
     operator fun unaryMinus() {
-        assertApproxEquals(DimensionalValue("2 g"), DimensionalValue("-2 g"), 1e-12)
+        assertApproxEquals(DimensionalValue("-2 g"), -DimensionalValue("2 g"), 1e-12)
     }
 
     @Test
