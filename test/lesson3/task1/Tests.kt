d2a000ffbfad97fee6ca95c4edab1fee23a865bb
Mikhail Glukhikh <glukhikh@mail.ru>
2021-08-18 16:34:20 +0300

    Drop task squareBetweenExists, see #100

--- test/lesson3/task1/Tests.kt
+++ test/lesson3/task1/Tests.kt
@@ -159,19 +159,6 @@ class Tests {
 
     @Test
     @Tag("3")
-    fun squareBetweenExists() {
-        assertTrue(squareBetweenExists(1, 1))
-        assertTrue(squareBetweenExists(21, 28))
-        assertTrue(squareBetweenExists(36, 48))
-        assertTrue(squareBetweenExists(50, 64))
-        assertFalse(squareBetweenExists(51, 61))
-        assertFalse(squareBetweenExists(999, 1001))
-        assertTrue(squareBetweenExists(152374337, 152423715))
-        assertFalse(squareBetweenExists(2147395601, Int.MAX_VALUE))
-    }
-
-    @Test
-    @Tag("3")
     fun revert() {
         assertEquals(87431, revert(13478))
         assertEquals(0, revert(0))
