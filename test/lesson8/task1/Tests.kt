9274cbcfb6ab06b3efd4f1528b9847f187bf47ff
Mikhail Glukhikh <glukhikh@mail.ru>
2021-08-18 14:34:35 +0300

    Rename test (lineByPoints) according to base function name

--- test/lesson8/task1/Tests.kt
+++ test/lesson8/task1/Tests.kt
@@ -207,7 +207,7 @@ class Tests {
 
     @Test
     @Tag("3")
-    fun lineByPoint() {
+    fun lineByPoints() {
         assertApproxEquals(Line(Point(0.0, 0.0), PI / 2), lineByPoints(Point(0.0, 0.0), Point(0.0, 2.0)))
         assertApproxEquals(Line(Point(1.0, 1.0), PI / 4), lineByPoints(Point(1.0, 1.0), Point(3.0, 3.0)))
     }
